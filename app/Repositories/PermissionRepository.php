8ee83a03640b478b9206071eb7f260090e0d039f
Luis Chilembo Mateus <luischilembomateus@gmail.com>
2024-07-27 14:25:30 +0100

    lista usuarios da API Laravel

---
+++ app/Repositories/PermissionRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Permission;
+
+class PermissionRepository
+{
+
+    public function __construct(protected Permission $permission)
+    {
+
+    }
+}
